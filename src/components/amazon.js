c5480a1ef975173ed26d5ed43eeee4b4fa593b85
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-23 23:07:22 +0600

    Inventory API added

--- src/components/amazon.js
+++ src/components/amazon.js
@@ -5,6 +5,7 @@ import "../styles/amazon.css";
 
 const Amazon = ({ handleClick }) => {
   const [products, setProducts] = useState([]);
+  const [inventory, setInventory] = useState([]);
 
   useEffect(() => {
     fetch("https://fec-inventory-api.herokuapp.com/product-info")
@@ -12,9 +13,18 @@ const Amazon = ({ handleClick }) => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    fetch("https://fec-inventory-api.herokuapp.com/inventory-info")
+      .then((res) => res.json())
+      .then((data) => setInventory(data));
+  }, []);
+
   return (
     <section>
       {products.map((item) => (
+        <Cards key={item.id} item={item} handleClick={handleClick} />
+      ))}
+      {inventory.map((item) => (
         <Cards key={item.id} item={item} handleClick={handleClick} />
       ))}
     </section>
